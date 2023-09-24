167fad792db96fa00bc16bae64a57b1ca1ab3456
sebastian Montes <u201712366@upc.edu.pe>
2023-09-24 18:35:42 -0500

    feat: Register Desing feature

--- FlexDormAngularWebApp/src/app/components/register/register.component.ts
+++ FlexDormAngularWebApp/src/app/components/register/register.component.ts
@@ -10,23 +10,42 @@ export class RegisterComponent {
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   student = true;
-  arrendar =false;
-  studentfalse(){
-  this.student=true
-  this.arrendar=false
-  const miBoton = this.el.nativeElement.querySelector('#studentboton');
-  this.renderer.setStyle(miBoton, 'background-color', '#670abe');
-  const miBoton2 = this.el.nativeElement.querySelector('#arrendarboton');
-  this.renderer.setStyle(miBoton2, 'background-color', 'withe');
-  }
-  arrendarfalse(){
-    this.arrendar=true
-    this.student=false
+  arrendar = false;
+  ngOnInit(): void {
     const miBoton = this.el.nativeElement.querySelector('#studentboton');
-    this.renderer.setStyle(miBoton, 'background-color', 'withe');
+    this.renderer.setStyle(miBoton, 'background-color', '#670abe');
+    const miBoton2 = this.el.nativeElement.querySelector('#arrendarboton');
+    this.renderer.setStyle(miBoton2, 'color', 'black');
+  }
+
+  studentfalse() {
+    this.student = true;
+    this.arrendar = false;
+    const miBoton = this.el.nativeElement.querySelector('#studentboton');
+    this.renderer.setStyle(miBoton, 'background-color', '#670abe');
+    this.renderer.setStyle(miBoton, 'color', 'white');
+    const miBoton2 = this.el.nativeElement.querySelector('#arrendarboton');
+    this.renderer.setStyle(miBoton2, 'background-color', 'white'); // Cambié 'withe' a 'white'
+    this.renderer.setStyle(miBoton2, 'color', 'black');
+  }
+
+  arrendarfalse() {
+    this.arrendar = true;
+    this.student = false;
+    const miBoton = this.el.nativeElement.querySelector('#studentboton');
+    this.renderer.setStyle(miBoton, 'background-color', 'white');
+    this.renderer.setStyle(miBoton, 'color', 'black');
     const miBoton2 = this.el.nativeElement.querySelector('#arrendarboton');
     this.renderer.setStyle(miBoton2, 'background-color', '#670abe');
+    this.renderer.setStyle(miBoton2, 'color', 'white');
   }
 
 
+  selectedDate: Date | undefined;  // Variable para almacenar la fecha seleccionada
+
+  onDateSelected(event: any) {
+    this.selectedDate = event.value; // Captura la fecha seleccionada
+    console.log('Fecha seleccionada:', this.selectedDate);
+    // Aquí puedes realizar acciones adicionales con la fecha seleccionada
+  }
 }
